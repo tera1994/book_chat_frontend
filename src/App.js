31d80c519ceb3632d6c2541b2b44706c28912036
tera <>
2024-03-17 09:58:13 +0900

    Add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import './App.css';
-import BookChatRoomList from "./pages/bookChatRoomList";
-import BookChatRoom from "./pages/bookChatRoom";
+import BookChatRoomList from "./pages/BookChatRoomList";
+import BookChatRoom from "./pages/BookChatRoom";
+import Header from "./components/Header"
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/book-chat-room-list" element={<BookChatRoomList />} />
+        <Route path="/" element={<BookChatRoomList />} />
         <Route path="/book-chat-room/:id" element={<BookChatRoom />} />
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <Link to="/" className="logo">
+          Book Chat
+        </Link>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <style jsx>
+        {`
+          .header {
+            background-color: #0067C0;
+            color: #fff;
+            padding: 1rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+          }
+
+          .logo {
+            font-size: 36px;
+            font-weight: bold;
+            text-decoration: none;
+            color: #fff;
+          }
+
+          .nav-links {
+            display: flex;
+          }
+
+          .nav-links li {
+            margin-left: 1.5rem;
+            list-style: none;
+          }
+
+          .nav-links li a {
+            text-decoration: none;
+            color: #fff;
+            transition: color 0.3s ease;
+          }
+
+          .nav-links li a:hover {
+            color: #aaa;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Header;
